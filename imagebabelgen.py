9e6e22ff261b25a78ccc823e7164258fb5fe2de7
alanh90 <ahourmand90@gmail.com>
2024-04-01 13:22:37 -0400

    Added a gui file

--- imagebabelgen.py
+++ imagebabelgen.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import base64
 
 class ImageBabelGenerator:
     def __init__(self, width, height, color_depth):
@@ -44,12 +45,47 @@ class ImageBabelGenerator:
         index = int(digits, base=self.color_depth)
         return index
 
+    def _index_to_string_id(self, index):
+        """
+        Convert a numerical index to a base-64 encoded string ID.
+        """
+        digits = np.base_repr(index, base=self.color_depth)
+        digits = digits.zfill(self.width * self.height)
+        binary_data = ''.join(format(int(d), '08b') for d in digits)
+        string_id = base64.b64encode(binary_data.encode()).decode()
+        return string_id
+
+    def _string_id_to_index(self, string_id):
+        """
+        Convert a base-64 encoded string ID to a numerical index.
+        """
+        binary_data = base64.b64decode(string_id).decode()
+        digits = [int(binary_data[i:i+8], 2) for i in range(0, len(binary_data), 8)]
+        index = int(''.join(str(d) for d in digits), base=self.color_depth)
+        return index
+
     def get_image_id(self, image):
         """
         Get the index (ID) of a given image in the sequence of all possible images.
         """
         index = self._image_to_index(image)
         return index
+
+    def get_image_string_id(self, image):
+        """
+        Get the base-64 encoded string ID of a given image.
+        """
+        index = self._image_to_index(image)
+        string_id = self._index_to_string_id(index)
+        return string_id
+
+    def generate_image_from_string_id(self, string_id):
+        """
+        Generate an image from a base-64 encoded string ID.
+        """
+        index = self._string_id_to_index(string_id)
+        image = self._index_to_image(index)
+        return image
 
     def set_parameters(self, width=None, height=None, color_depth=None):
         """
@@ -86,3 +122,13 @@ class ImageBabelGenerator:
         h_flipped = np.fliplr(pixels)
         v_flipped = np.flipud(pixels)
         return np.array_equal(pixels, h_flipped) or np.array_equal(pixels, v_flipped)
+
+    def find_next_nonrandom_image(self, threshold=0.7):
+        current_index = self.current_index
+        while current_index < self.total_images:
+            image = self._index_to_image(current_index)
+            if not self.is_random_noise(image, threshold):
+                self.current_index = current_index
+                return image
+            current_index += 1
+        return None
--- imagebabelgui.py
+++ imagebabelgui.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 import numpy as np
 from imagebabelgen import ImageBabelGenerator
+import os
 
 
 class ImageBabelGUI:
@@ -30,11 +31,76 @@ class ImageBabelGUI:
         self.button_next = tk.Button(self.button_frame, text="Next", command=self.next_image)
         self.button_next.pack(side=tk.LEFT)
 
+        self.button_step_prev = tk.Button(self.button_frame, text="Step Prev", command=self.step_prev)
+        self.button_step_prev.pack(side=tk.LEFT)
+
+        self.button_step_next = tk.Button(self.button_frame, text="Step Next", command=self.step_next)
+        self.button_step_next.pack(side=tk.LEFT)
+
+        self.button_auto = tk.Button(self.button_frame, text="Auto", command=self.toggle_auto)
+        self.button_auto.pack(side=tk.LEFT)
+
         self.button_import = tk.Button(self.button_frame, text="Import Image", command=self.import_image)
         self.button_import.pack(side=tk.LEFT)
 
+        self.button_save = tk.Button(self.button_frame, text="Save Image", command=self.save_image)
+        self.button_save.pack(side=tk.LEFT)
+
+        self.label_width = tk.Label(master, text="Width:")
+        self.label_width.pack()
+        self.entry_width = tk.Entry(master)
+        self.entry_width.pack()
+
+        self.label_height = tk.Label(master, text="Height:")
+        self.label_height.pack()
+        self.entry_height = tk.Entry(master)
+        self.entry_height.pack()
+
+        self.label_color_depth = tk.Label(master, text="Color Depth:")
+        self.label_color_depth.pack()
+        self.entry_color_depth = tk.Entry(master)
+        self.entry_color_depth.pack()
+
+        self.button_generate = tk.Button(master, text="Generate Images", command=self.generate_images)
+        self.button_generate.pack()
+
+        self.label_step_size = tk.Label(master, text="Step Size:")
+        self.label_step_size.pack()
+        self.entry_step_size = tk.Entry(master)
+        self.entry_step_size.insert(tk.END, "1")
+        self.entry_step_size.pack()
+        self.entry_step_size.bind("<Return>", self.update_step_size)
+
+        self.button_fast_forward = tk.Button(self.button_frame, text="Fast Forward", command=self.fast_forward)
+        self.button_fast_forward.pack(side=tk.LEFT)
+
+        self.button_save_nonrandom = tk.Button(self.button_frame, text="Save Non-Random", command=self.save_nonrandom)
+        self.button_save_nonrandom.pack(side=tk.LEFT)
+
+        self.label_threshold = tk.Label(master, text="Randomness Threshold:")
+        self.label_threshold.pack()
+        self.entry_threshold = tk.Entry(master)
+        self.entry_threshold.insert(tk.END, "0.7")
+        self.entry_threshold.pack()
+
+        self.auto_increment = False
+        self.step_size = 1
+
         # Display the first image
         self.update_image(0)
+
+    def fast_forward(self):
+        current_index = self.slider.get()
+        fast_forward_step = self.step_size * 100  # Adjust the factor as needed
+        if current_index + fast_forward_step < self.generator.get_total_images():
+            self.slider.set(current_index + fast_forward_step)
+
+    def save_nonrandom(self):
+        threshold = float(self.entry_threshold.get())
+        image = self.generator.find_next_nonrandom_image(threshold)
+        if image is not None:
+            self.update_image(self.generator.current_index)
+            self.save_image()
 
     def update_image(self, index):
         image = self.generator.generate_image(int(index))
@@ -53,6 +119,40 @@ class ImageBabelGUI:
         if current_index < self.generator.get_total_images() - 1:
             self.slider.set(current_index + 1)
 
+    def step_prev(self):
+        current_index = self.slider.get()
+        if current_index - self.step_size >= 0:
+            self.slider.set(current_index - self.step_size)
+
+    def step_next(self):
+        current_index = self.slider.get()
+        if current_index + self.step_size < self.generator.get_total_images():
+            self.slider.set(current_index + self.step_size)
+
+    def toggle_auto(self):
+        self.auto_increment = not self.auto_increment
+        if self.auto_increment:
+            self.button_auto.config(text="Stop")
+            self.auto_generate()
+        else:
+            self.button_auto.config(text="Auto")
+
+    def auto_generate(self):
+        if self.auto_increment:
+            current_index = self.slider.get()
+            if current_index + self.step_size < self.generator.get_total_images():
+                self.slider.set(current_index + self.step_size)
+                self.master.after(100, self.auto_generate)  # Adjust the delay as needed
+            else:
+                self.auto_increment = False
+                self.button_auto.config(text="Auto")
+
+    def update_step_size(self, *args):
+        try:
+            self.step_size = int(self.entry_step_size.get())
+        except ValueError:
+            pass
+
     def import_image(self):
         file_path = filedialog.askopenfilename()
         if file_path:
@@ -62,3 +162,16 @@ class ImageBabelGUI:
             image_id = self.generator.get_image_id(image)
             self.slider.set(image_id)
 
+    def save_image(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".png")
+        if file_path:
+            image = self.generator.generate_image(self.slider.get())
+            image.save(file_path)
+
+    def generate_images(self):
+        width = int(self.entry_width.get())
+        height = int(self.entry_height.get())
+        color_depth = int(self.entry_color_depth.get())
+        self.generator.set_parameters(width, height, color_depth)
+        self.slider.config(to=self.generator.get_total_images() - 1)
+        self.update_image(0)
